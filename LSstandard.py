6a4d48c1b1b703c0c64165e382fd03a190738a81
Lorenzo Stigliano <59342085+MaxBubblegum47@users.noreply.github.com>
2020-11-20 20:22:47 +0100

    Update LSstandard.py

--- LSstandard.py
+++ LSstandard.py
@@ -2,7 +2,7 @@ from tkinter import *
 import os
 import json
 import lyricsgenius
-genius = lyricsgenius.Genius("dKdv-UTxwXPwypUw5d9n-ZJz9CTxK3R7V0D5BouXuGhfGQpgU8KASPgr6gw00qTm")
+genius = lyricsgenius.Genius("Enter Toker Here")
 import nltk
 import tkinter as tk
 from tkinter import filedialog
